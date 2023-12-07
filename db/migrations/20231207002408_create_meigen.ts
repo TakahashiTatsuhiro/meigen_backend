06e78294e61332fb1d787a1fa299e77caf95f8c4
tatsu <tatsuhiro_takahashi_aa@mail.toyota.co.jp>
2023-12-07 14:43:25 +0900

    deploy10

--- db/migrations/20231207002408_create_meigen.ts
+++ db/migrations/20231207002408_create_meigen.ts
@@ -2,15 +2,15 @@ import { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
-    return knex.schema.createTable('meigen', function (table) {
+    return knex.schema.createTable('quote', function (table) {
 		table.increments('id').primary();
 		table.string('author').notNullable();
-		table .text('meigen').notNullable();
+		table .text('body').notNullable();
 	});
 }
 
 
 export async function down(knex: Knex): Promise<void> {
-    return knex.schema.dropTable('meigen');
+    return knex.schema.dropTable('quote');
 }
 
